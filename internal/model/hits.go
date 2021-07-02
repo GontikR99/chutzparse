081a1095e11f09b6921909819843a3701e3f3a9a
GontikR99 <gontikr99@gmail.com>
2021-07-01 23:43:42 -0400

    Added pet damage to floating hit display

--- internal/model/hits.go
+++ internal/model/hits.go
@@ -4,6 +4,7 @@ package model
 
 import (
 	"github.com/gontikr99/chutzparse/internal/eqlog"
+	"github.com/gontikr99/chutzparse/internal/model/iff"
 	"github.com/gontikr99/chutzparse/internal/model/parsedefs"
 	"github.com/gontikr99/chutzparse/internal/presenter"
 )
@@ -51,6 +52,34 @@ func listenForHits() {
 					presenter.BroadcastHitEvent(presenter.ChannelHitBottom, &presenter.HitEvent{
 						Text:  parsedefs.RenderAmount(float64(dmgEntry.Amount)),
 						Color: "red",
+						Big:   dmgEntry.Flag&eqlog.CriticalFlag != 0,
+					})
+				} else if iff.GetOwner(dmgEntry.Source)==entry.Character {
+					// pet doing damage
+					if dmgEntry.SpellName!="" {
+						presenter.BroadcastHitEvent(presenter.ChannelHitTop, &presenter.HitEvent{
+							Text:  parsedefs.RenderAmount(float64(dmgEntry.Amount)),
+							Color: "khaki",
+							Big:   dmgEntry.Flag&eqlog.CriticalFlag != 0,
+						})
+					} else if dmgEntry.Element == eqlog.PhysicalDamage {
+						presenter.BroadcastHitEvent(presenter.ChannelHitTop, &presenter.HitEvent{
+							Text:  parsedefs.RenderAmount(float64(dmgEntry.Amount)),
+							Color: "bisque",
+							Big:   dmgEntry.Flag&eqlog.CriticalFlag != 0,
+						})
+					} else {
+						presenter.BroadcastHitEvent(presenter.ChannelHitTop, &presenter.HitEvent{
+							Text:  parsedefs.RenderAmount(float64(dmgEntry.Amount)),
+							Color: "gray",
+							Big:   dmgEntry.Flag&eqlog.CriticalFlag != 0,
+						})
+					}
+				} else if iff.GetOwner(dmgEntry.Target) == entry.Character {
+					// incoming damage
+					presenter.BroadcastHitEvent(presenter.ChannelHitBottom, &presenter.HitEvent{
+						Text:  parsedefs.RenderAmount(float64(dmgEntry.Amount)),
+						Color: "indianred",
 						Big:   dmgEntry.Flag&eqlog.CriticalFlag != 0,
 					})
 				}
