e6f3615a8ebeccb5e95631c461c9dc1b196b5dac
gontikr99 <gontikr99@gmail.com>
2021-07-13 15:57:53 -0400

    Refactored RPC stub generation code into better separated concerns.

---
+++ internal/cmd/rpcgen/comprehend.go
@@ -0,0 +1,236 @@
+// +build native
+
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"io"
+	"path"
+)
+
+type GenerationContext struct {
+	NeededImports []Import
+}
+
+type Import struct {
+	Name string
+	Path string
+}
+
+// ImportName returns the shorthand name we'll use in the generated file to refer to a specific import path
+func (gc *GenerationContext) ImportName(impPath string) string {
+	for _, imRec := range gc.NeededImports {
+		if imRec.Path== impPath {
+			return imRec.Name
+		}
+	}
+	base := path.Base(impPath)
+	idx := 1
+	for {
+		storeName := base
+		if idx!=1 {
+			storeName=fmt.Sprintf("%s%d", base, idx)
+		}
+		free := true
+		for _, imRec := range gc.NeededImports {
+			if imRec.Name == base {
+				free = false
+				break
+			}
+		}
+		if free {
+			gc.NeededImports = append(gc.NeededImports, Import{
+				Name: storeName,
+				Path: impPath,
+			})
+			return storeName
+		}
+	}
+}
+
+func (gc *GenerationContext) WriteImports(out io.Writer) error {
+	_, err := fmt.Fprintln(out, "import (")
+	if err!=nil {return err}
+
+	for _, entry := range gc.NeededImports {
+		if path.Base(entry.Path) == entry.Name {
+			_, err = fmt.Fprintf(out,"    \"%s\"\n", entry.Path)
+			if err!=nil {
+				return err
+			}
+		} else {
+			_, err = fmt.Fprintf(out, "    %s \"%s\"\n", entry.Name, entry.Path)
+			if err!=nil {
+				return err
+			}
+		}
+	}
+	_, err = fmt.Fprintln(out, ")")
+	return err
+}
+
+type ComprehendedSource struct {
+	fileSet *token.FileSet
+	parsedFile *ast.File
+	importMap map[string]string
+	interfaces map[string]Interface
+}
+
+func ReadGoSource(fileName string) (*ComprehendedSource, error) {
+	result := &ComprehendedSource{
+		fileSet: token.NewFileSet(),
+		importMap: map[string]string{},
+		interfaces: map[string]Interface{},
+	}
+	var err error
+	result.parsedFile, err = parser.ParseFile(result.fileSet, fileName, nil, 0)
+	if err!=nil {
+		return nil, err
+	}
+
+	// parse imports
+	for _, ispec := range result.parsedFile.Imports {
+		imPath := ispec.Path.Value[1:len(ispec.Path.Value)-1]
+		imName := path.Base(imPath)
+		if ispec.Name==nil {
+			imName = ispec.Name.String()
+		}
+		result.importMap[imName]=imPath
+	}
+
+	// parse interfaces
+	for _, decl := range result.parsedFile.Decls {
+		genDec, ok := decl.(*ast.GenDecl)
+		if !ok {continue}
+		if len(genDec.Specs)<1 {
+			continue
+		}
+		if _, ok = genDec.Specs[0].(*ast.ImportSpec); ok {
+			continue
+		}
+		if _, ok = genDec.Specs[0].(*ast.ValueSpec); ok {
+			continue
+		}
+		if len(genDec.Specs)>1 {
+			return nil, fmt.Errorf("multiple spec in declaration at %v", genDec.Pos())
+		}
+		typeSpec, ok := genDec.Specs[0].(*ast.TypeSpec)
+		if !ok {
+			return nil, fmt.Errorf("expected TypeSpec at %v", genDec.Pos())
+		}
+		ifType, ok := typeSpec.Type.(*ast.InterfaceType)
+		if !ok {
+			continue
+		}
+		ifName := typeSpec.Name.Name
+
+		methods := Interface{}
+		for _, method := range ifType.Methods.List {
+			ifMethod := InterfaceMethod{}
+			if len(method.Names)!=1 {
+				return nil, fmt.Errorf("expected a single name per method at %v", method.Pos())
+			}
+			methodName := method.Names[0].String()
+			fun, ok := method.Type.(*ast.FuncType)
+			if !ok {
+				return nil, fmt.Errorf("expected method to be a function type at %v", method.Pos())
+			}
+			for idx, arg := range fun.Params.List {
+				fArg := FuncParameter{
+					Name: fmt.Sprintf("arg%d", idx),
+					Type: arg.Type,
+					SourceFile: result,
+				}
+				if len(arg.Names)==1 {
+					fArg.Name=arg.Names[0].Name
+				}
+				ifMethod.ArgTypes = append(ifMethod.ArgTypes, fArg)
+			}
+			for idx, ret := range fun.Results.List {
+				rArg := FuncParameter{
+					Name: fmt.Sprintf("ret%d", idx),
+					Type: ret.Type,
+					SourceFile: result,
+				}
+				if len(ret.Names)==1 {
+					rArg.Name=ret.Names[0].Name
+				}
+				ifMethod.RetTypes = append(ifMethod.RetTypes, rArg)
+			}
+			ifMethod.RetTypes = ifMethod.RetTypes[:len(ifMethod.RetTypes)-1]
+			methods[methodName] = ifMethod
+		}
+		result.interfaces[ifName]=methods
+	}
+	return result, nil
+}
+
+func (cs *ComprehendedSource) PackageName() string {
+	return cs.parsedFile.Name.Name
+}
+
+type FuncParameter struct {
+	Name string
+	Type ast.Expr
+	SourceFile *ComprehendedSource
+}
+
+func formatType(gc *GenerationContext, cs *ComprehendedSource, expr ast.Expr) string {
+	if idExp, ok := expr.(*ast.Ident); ok {
+		return idExp.String()
+	}
+	if starExp, ok := expr.(*ast.StarExpr); ok {
+		return "*"+formatType(gc, cs, starExp.X)
+	}
+	if arrExp, ok := expr.(*ast.ArrayType); ok {
+		// FIXME: handle arrays in addition to slices
+		return "[]"+formatType(gc, cs, arrExp.Elt)
+	}
+	if mapExp, ok := expr.(*ast.MapType); ok {
+		return "map["+formatType(gc, cs, mapExp.Key)+"]"+formatType(gc, cs, mapExp.Value)
+	}
+	if selExp, ok := expr.(*ast.SelectorExpr); ok {
+		pkg := selExp.X.(*ast.Ident).Name
+		pkgPath := cs.importMap[pkg]
+		impName := gc.ImportName(pkgPath)
+		return impName+"."+selExp.Sel.Name
+	}
+	// FIXME: handle function types (*ast.FuncType), even though gob and net/rpc can't transport them?
+	panic(fmt.Errorf("unknown expression type at %v", expr.Pos()))
+}
+
+func (fp FuncParameter) TypeText(gc *GenerationContext) string {
+	return formatType(gc, fp.SourceFile, fp.Type)
+}
+
+type InterfaceMethod struct {
+	ArgTypes []FuncParameter
+	RetTypes []FuncParameter
+}
+
+type Interface map[string]InterfaceMethod
+
+type ImportSet []Import
+
+func (is *ImportSet) Add(ispec *ast.ImportSpec) {
+	iport := Import{}
+	iport.Path = ispec.Path.Value[1:len(ispec.Path.Value)-1]
+	if ispec.Name==nil {
+		iport.Name=path.Base(iport.Path)
+	} else {
+		iport.Name=ispec.Name.String()
+	}
+	*is=append(*is, iport)
+}
+
+func (is *ImportSet) Lookup(abbrev string) string {
+	for _, importEntry := range *is {
+		if importEntry.Name == abbrev {
+			return importEntry.Path
+		}
+	}
+	return abbrev
+}
--- internal/cmd/rpcgen/generate.go
+++ internal/cmd/rpcgen/generate.go
@@ -7,75 +7,11 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"go/ast"
-	"go/parser"
-	"go/token"
 	"io/ioutil"
 	"os"
 	"path"
 	"strings"
 )
-
-type FuncArg struct {
-	Name string
-	Type string
-}
-
-type InterfaceMethod struct {
-	Name string
-	ArgTypes []FuncArg
-	RetTypes []FuncArg
-}
-
-type Import struct {
-	Name string
-	Path string
-}
-
-type ImportSet []Import
-
-func (is *ImportSet) Add(ispec *ast.ImportSpec) {
-	iport := Import{}
-	iport.Path = ispec.Path.Value[1:len(ispec.Path.Value)-1]
-	if ispec.Name==nil {
-		iport.Name=path.Base(iport.Path)
-	} else {
-		iport.Name=ispec.Name.String()
-	}
-	*is=append(*is, iport)
-}
-
-func (is *ImportSet) Lookup(abbrev string) string {
-	for _, importEntry := range *is {
-		if importEntry.Name == abbrev {
-			return importEntry.Path
-		}
-	}
-	return abbrev
-}
-
-// formatType takes an ast.Expr representing a type and formats it into something that
-// can be written to a generated file.  In the process, it updates reqimports with any
-// imports from the original file that would need to be copied into the output file.
-func formatType(reqimports map[string]struct{}, expr ast.Expr) string {
-	if selExp, ok := expr.(*ast.SelectorExpr); ok {
-		pkg := selExp.X.(*ast.Ident).String()
-		reqimports[pkg]=struct{}{}
-		return pkg+"."+selExp.Sel.String()
-	}
-	if idExp, ok := expr.(*ast.Ident); ok {
-		return idExp.String()
-	}
-	if starExp, ok := expr.(*ast.StarExpr); ok {
-		return "*"+formatType(reqimports, starExp.X)
-	}
-	if arrExp, ok := expr.(*ast.ArrayType); ok {
-		// FIXME: handle fixed length arrays
-		return "[]"+formatType(reqimports, arrExp.Elt)
-	}
-	// FIXME: handle function types (*ast.FuncType), even though gob and net/rpc can't transport them?
-	panic(fmt.Errorf("unknown expression type at %v", expr.Pos()))
-}
 
 func upperCamlCase(name string) string {
 	if len(name)>0 {
@@ -86,7 +22,6 @@ func upperCamlCase(name string) string {
 }
 
 func main() {
-	fset := token.NewFileSet()
 	inputFilename := os.Args[1]
 	inputBasename := path.Base(inputFilename)
 	if strings.HasSuffix(inputBasename, ".go") {
@@ -94,196 +29,117 @@ func main() {
 	}
 	outputBasename := inputBasename+"_genrpc.go"
 	outputFilename := path.Join(path.Dir(inputFilename), outputBasename)
-	parsedFile, err := parser.ParseFile(fset, inputFilename, nil, 0)
+
+	compFile, err := ReadGoSource(inputFilename)
 	if err!=nil {
 		panic(err)
 	}
 
-	imports := new(ImportSet)
-	for _, ispec := range parsedFile.Imports {
-		imports.Add(ispec)
-	}
-	neededImports := map[string]struct{}{}
+	mainCode := &bytes.Buffer{}
+	gc := &GenerationContext{}
 
-	if err!=nil {
-		panic(err)
-	}
+	for ifName, methods := range compFile.interfaces {
+		// Write stub declarations
+		fmt.Fprintf(mainCode, "type Stub_serverSide_%s struct {impl %s}\n", ifName, ifName)
+		fmt.Fprintf(mainCode, "type Stub_clientSide_%s struct {client *%s.Client}\n", ifName, gc.ImportName("net/rpc"))
 
-	ifName:=""
-	var ifType *ast.InterfaceType
-
-	for _, decl := range parsedFile.Decls {
-		genDec, ok := decl.(*ast.GenDecl)
-		if !ok {continue}
-		if len(genDec.Specs)<1 {
-			continue
-		}
-		if _, ok = genDec.Specs[0].(*ast.ImportSpec); ok {
-			continue
-		}
-		if _, ok = genDec.Specs[0].(*ast.ValueSpec); ok {
-			continue
-		}
-		if len(genDec.Specs)>1 {
-			panic(fmt.Errorf("multiple spec in declaration at %v", genDec.Pos()))
-		}
-		typeSpec, ok := genDec.Specs[0].(*ast.TypeSpec)
-		if !ok {
-			panic(fmt.Errorf("expected TypeSpec at %v", genDec.Pos()))
-		}
-		ifVal, ok := typeSpec.Type.(*ast.InterfaceType)
-		if !ok {
-			continue
-		}
-		if ifName=="" {
-			ifName = typeSpec.Name.String()
-			ifType = ifVal
-		} else {
-			panic(fmt.Errorf("more than one interface found at %v", genDec.Pos()))
-		}
-	}
-
-	methods := []*InterfaceMethod{}
-	for _, method := range ifType.Methods.List {
-		ifMethod := &InterfaceMethod{}
-		if len(method.Names)!=1 {
-			panic(fmt.Errorf("expected a single name per method at %v", method.Pos()))
-		}
-		ifMethod.Name = method.Names[0].String()
-		fun, ok := method.Type.(*ast.FuncType)
-		if !ok {
-			panic(fmt.Errorf("expected method to be a function type at %v", method.Pos()))
-		}
-		for idx, arg := range fun.Params.List {
-			fArg := FuncArg{
-				Name: fmt.Sprintf("Arg%d", idx),
-				Type: formatType(neededImports, arg.Type),
+		// Create stub implementations
+		for methodName, method := range methods {
+			fmt.Fprintf(mainCode, "type RequestMessage_%s_%s struct {\n", ifName, methodName)
+			for _, field := range method.ArgTypes {
+				fmt.Fprintf(mainCode, "  %s %s\n", upperCamlCase(field.Name), field.TypeText(gc))
 			}
-			if len(arg.Names)==1 {
-				fArg.Name=upperCamlCase(arg.Names[0].Name)
+			fmt.Fprintln(mainCode, "}")
+
+			fmt.Fprintf(mainCode, "type ResponseMessage_%s_%s struct {\n", ifName, methodName)
+			for _, field := range method.RetTypes {
+				fmt.Fprintf(mainCode, "  %s %s\n", upperCamlCase(field.Name), field.TypeText(gc))
 			}
-			ifMethod.ArgTypes = append(ifMethod.ArgTypes, fArg)
-		}
-		for idx, ret := range fun.Results.List {
-			rArg := FuncArg{
-				Name: fmt.Sprintf("Ret%d", idx),
-				Type: formatType(neededImports, ret.Type),
+			fmt.Fprintln(mainCode, "}")
+
+			fmt.Fprintf(mainCode, "func (ss Stub_serverSide_%s) %s(req *RequestMessage_%s_%s, res *ResponseMessage_%s_%s) error {\n",
+				ifName,
+				methodName,
+				ifName, methodName,
+				ifName, methodName,
+			)
+			fmt.Fprintln(mainCode, "  var err error")
+			mainCode.WriteString("  ")
+			for _, field := range method.RetTypes {
+				fmt.Fprintf(mainCode, "res.%s, ", upperCamlCase(field.Name))
 			}
-			if len(ret.Names)==1 {
-				rArg.Name=upperCamlCase(ret.Names[0].Name)
+			fmt.Fprintf(mainCode, "err = ss.impl.%s(", methodName)
+			needSep := false
+			for _, field := range method.ArgTypes {
+				if needSep {
+					mainCode.WriteString(", ")
+				} else {
+					needSep = true
+				}
+				fmt.Fprintf(mainCode, "req.%s", upperCamlCase(field.Name))
 			}
-			ifMethod.RetTypes = append(ifMethod.RetTypes, rArg)
-		}
-		if len(ifMethod.RetTypes)==0 || ifMethod.RetTypes[len(ifMethod.RetTypes)-1].Type!="error" {
-			panic(fmt.Errorf("Method %s must return 'error' as its last result type at %v", ifMethod.Name, method.Pos()))
-		}
-		ifMethod.RetTypes = ifMethod.RetTypes[:len(ifMethod.RetTypes)-1]
-		methods=append(methods, ifMethod)
-	}
+			fmt.Fprintln(mainCode, ")")
+			fmt.Fprintln(mainCode, "  return err")
+			fmt.Fprintln(mainCode, "}")
 
-	output := &bytes.Buffer{}
-
-	// Write package
-	fmt.Fprintf(output, "package %s\n", parsedFile.Name.String())
-	fmt.Fprintln(output)
-
-	// Write warning
-	fmt.Fprintln(output, "// This code is generated, do not edit!")
-
-	// Write imports
-	if len(neededImports)!=0 {
-		fmt.Fprintln(output, "import (")
-		for impName, _ := range neededImports {
-			fmt.Fprintf(output, "  %s \"%s\"\n", impName, imports.Lookup(impName))
-		}
-		fmt.Fprintln(output, ")")
-	}
-	fmt.Fprintln(output, "import (golang_net_rpc \"net/rpc\")")
-	fmt.Fprintln(output)
-
-	// Write stub declarations
-	fmt.Fprintf(output, "type Stub_serverSide_%s struct {impl %s}\n", ifName, ifName)
-	fmt.Fprintf(output, "type Stub_clientSide_%s struct {client *golang_net_rpc.Client}\n", ifName)
-
-	// Create stub implementations
-	for _, method := range methods {
-		fmt.Fprintf(output, "type RequestMessage_%s_%s struct {\n", ifName, method.Name)
-		for _, field := range method.ArgTypes {
-			fmt.Fprintf(output, "  %s %s\n", field.Name, field.Type)
-		}
-		fmt.Fprintln(output, "}")
-
-		fmt.Fprintf(output, "type ResponseMessage_%s_%s struct {\n", ifName, method.Name)
-		for _, field := range method.RetTypes {
-			fmt.Fprintf(output, "  %s %s\n", field.Name, field.Type)
-		}
-		fmt.Fprintln(output, "}")
-
-		fmt.Fprintf(output, "func (ss Stub_serverSide_%s) %s(req *RequestMessage_%s_%s, res *ResponseMessage_%s_%s) error {\n",
-			ifName,
-			method.Name,
-			ifName, method.Name,
-			ifName, method.Name,
-		)
-		fmt.Fprintln(output, "  var err error")
-		output.WriteString("  ")
-		for _, field := range method.RetTypes {
-			fmt.Fprintf(output, "res.%s, ", field.Name)
-		}
-		fmt.Fprintf(output, "err = ss.impl.%s(", method.Name)
-		needSep := false
-		for _, field := range method.ArgTypes {
-			if needSep {
-				output.WriteString(", ")
-			} else {
-				needSep = true
+			fmt.Fprintf(mainCode, "func (cs Stub_clientSide_%s) %s(", ifName, methodName)
+			needSep = false
+			for idx, field := range method.ArgTypes {
+				if needSep {
+					mainCode.WriteString(", ")
+				} else {
+					needSep = true
+				}
+				fmt.Fprintf(mainCode, "arg%d %s", idx, field.TypeText(gc))
 			}
-			fmt.Fprintf(output, "req.%s", field.Name)
-		}
-		fmt.Fprintln(output, ")")
-		fmt.Fprintln(output, "  return err")
-		fmt.Fprintln(output, "}")
-
-		fmt.Fprintf(output, "func (cs Stub_clientSide_%s) %s(", ifName, method.Name)
-		needSep = false
-		for idx, field := range method.ArgTypes {
-			if needSep {
-				output.WriteString(", ")
-			} else {
-				needSep = true
+			fmt.Fprintf(mainCode, ") (")
+			for _, rField := range method.RetTypes {
+				fmt.Fprintf(mainCode, "%s, ", rField.TypeText(gc))
 			}
-			fmt.Fprintf(output, "arg%d %s", idx, field.Type)
+			fmt.Fprintln(mainCode, "error) {")
+			fmt.Fprintf(mainCode, "  req := &RequestMessage_%s_%s{\n", ifName, methodName)
+			for idx, field := range method.ArgTypes {
+				fmt.Fprintf(mainCode, "    %s: arg%d,\n", upperCamlCase(field.Name), idx)
+			}
+			fmt.Fprintln(mainCode, "  }")
+			fmt.Fprintf(mainCode, "  res := new(ResponseMessage_%s_%s)\n", ifName, methodName)
+			fmt.Fprintf(mainCode, "  err := cs.client.Call(\"Stub_serverSide_%s.%s\", req, res)\n", ifName, methodName)
+			fmt.Fprintf(mainCode, "  return ")
+			for _, field := range method.RetTypes {
+				fmt.Fprintf(mainCode, "res.%s, ", upperCamlCase(field.Name))
+			}
+			fmt.Fprintln(mainCode, "err")
+			fmt.Fprintln(mainCode, "}")
 		}
-		fmt.Fprintf(output, ") (")
-		for _, rField := range method.RetTypes {
-			fmt.Fprintf(output, "%s, ", rField.Type)
-		}
-		fmt.Fprintln(output, "error) {")
-		fmt.Fprintf(output, "  req := &RequestMessage_%s_%s{\n", ifName, method.Name)
-		for idx, field := range method.ArgTypes {
-			fmt.Fprintf(output, "    %s: arg%d,\n", field.Name, idx)
-		}
-		fmt.Fprintln(output, "  }")
-		fmt.Fprintf(output, "  res := new(ResponseMessage_%s_%s)\n", ifName, method.Name)
-		fmt.Fprintf(output, "  err := cs.client.Call(\"Stub_serverSide_%s.%s\", req, res)\n", ifName, method.Name)
-		fmt.Fprintf(output, "  return ")
-		for _, field := range method.RetTypes {
-			fmt.Fprintf(output, "res.%s, ", field.Name)
-		}
-		fmt.Fprintln(output, "err")
-		fmt.Fprintln(output, "}")
-	}
-	fmt.Fprintf(output, `
-func New%sClient(client *golang_net_rpc.Client) %s {
+		fmt.Fprintf(mainCode, `
+func New%sClient(client *%s.Client) %s {
     return Stub_clientSide_%s{client}
 }
 
-func Handle%s(server %s) func(*golang_net_rpc.Server) {
+func Handle%s(server %s) func(*%s.Server) {
     ss:=Stub_serverSide_%s{server}
-    return func(srv *golang_net_rpc.Server) {srv.Register(ss)}
+    return func(srv *%s.Server) {srv.Register(ss)}
 }
-`, ifName, ifName, ifName, ifName, ifName, ifName)
-	err = ioutil.WriteFile(outputFilename, output.Bytes(), 0644)
+`, ifName, gc.ImportName("net/rpc"), ifName, ifName,
+ifName, ifName, gc.ImportName("net/rpc"), ifName, gc.ImportName("net/rpc"))
+	}
+
+	fullBuf := &bytes.Buffer{}
+
+	// Write package
+	fmt.Fprintf(fullBuf, "package %s\n", compFile.PackageName())
+	fmt.Fprintln(fullBuf)
+
+	// Write warning
+	fmt.Fprintln(fullBuf, "// This code is generated, do not edit!")
+
+	// Write imports
+	gc.WriteImports(fullBuf)
+
+	// Write code
+	fullBuf.Write(mainCode.Bytes())
+
+	err = ioutil.WriteFile(outputFilename, fullBuf.Bytes(), 0644)
 	if err!=nil {
 		panic(err)
 	}
